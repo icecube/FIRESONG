e0ed9839263a7768d99a194d89ff8c7b2ff9d7e2
Ignacio Taboada <itaboada@gatech.edu>
2016-09-08 13:14:00 -0400

    Gzip the output

--- RunManager.py
+++ RunManager.py
@@ -5,4 +5,6 @@ import os
 density = 1e-8
 
 for i in range(1,100):
-    os.system("./StandardCandleSFR.py -o Results/Firesong_Density_" + str(density) + "_" + str(i) + ".txt -d " + str(density) + " -p")
+    filename = "Results/Firesong_Density_" + str(density) + "_" + str(i).zfill(3) + ".txt"
+    os.system("./StandardCandleSFR.py -o " + filename + " -d " + str(density) + " -p")
+    os.system("gzip " + filename)
