b48ccb5cc5024ce901497485a7727332a742f59e
Ignacio Taboada <itaboada@gatech.edu>
2016-09-18 08:40:05 -0400

    Adding options for flux normalization and index

--- IceCubeEffectiveArea/evt_calculation.py
+++ IceCubeEffectiveArea/evt_calculation.py
@@ -3,8 +3,26 @@
 #	Calculate the number of observed events by carrying out numerical integration:
 #	N_evt = Sum{Flux * Exposure * dE * dSteradian} 
 
-
+import argparse
 import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--fluxnorm", action="store",
+                    dest="fluxnorm", default=0.9e-8,
+                    help="Flux normalization in A(E/100TeV)^-index units GeV.cm^-2.s^-1.sr^-1")
+parser.add_argument("--index", action="store",
+                    dest="index", default=2.13,
+                    help="Spectral index (positive)")
+options = parser.parse_args()
+index = float(options.index)
+fluxnorm = float(options.fluxnorm)
+
+print ("##############################################################################")
+print ("Diffuse flux event number calculation")
+print ("Using Aachen's Diffuse flux exposure above 200 TeV")
+print ("Spectrum assumed: " + str(options.fluxnorm) + "(E/100TeV)^-" + str(options.index) + " GeV.cm^-2.s^-1.sr^-1")
+
+
 
 #Get file name for all truncated data files
 filelist = open('filelist.txt', 'r')
@@ -36,7 +54,7 @@ for fn in filelist :
 	input.close()
 
 	#number of event per energy (i.e. flux * exposure * delta_steradian)
-	Devt = 1e4*0.5*0.90e-8*np.power(e_list/1e5, -0.13)*np.power(e_list, -2)*expose_list*2*np.pi*0.05
+	Devt = 1e4*0.5*fluxnorm*np.power(e_list/1e5, -(index-2.))*np.power(e_list, -2)*expose_list*2*np.pi*0.05
 
 	#get mid point of the number of event, too get more accurate result
 	Devt_mid = Devt[:-1] + np.diff(Devt)/2.
