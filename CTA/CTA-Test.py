bf94ed25c40d64aec254f6613b0e2bda3f67474c
Ignacio Taboada <itaboada@gatech.edu>
2016-11-23 17:01:22 -0500

    Beautification

--- CTA/CTA-Test.py
+++ CTA/CTA-Test.py
@@ -1,46 +1,54 @@
 #!/usr/bin/python
 #
-# Read Effective area for CTA
-#
-#
 import re
 import numpy as np
 import math
 import EBL
+import argparse
 
-eblfilename = 'EBL_Gilmore.txt'
-ebl = EBL.EBL(eblfilename)
+#
+# Process command line options
+#
+parser = argparse.ArgumentParser()
+parser.add_argument('--ObsTime', action='store', dest='hours', default="50",
+                    help='Observation time in hours')
+parser.add_argument("--index", action="store", dest='index', type=float, default=-2.1,
+                    help="Spectral Index")
+parser.add_argument("--fluxnorm", action="store", dest='fluxnorm', type=float, default=1e-12,
+                    help="Flux normalization")
+parser.add_argument("--redshift", action="store", dest='redshift', type=float, default=0.,
+                    help="Redshift")
+options = parser.parse_args()
 
-#bckname = "CTA-Performance-North-5h-Background.txt"
-#bckg = np.loadtxt(bckname)
-#background = 0
-#for i in range(0,len(bckg)):
-#    background = background + bckg[i][2] * 3600 * 5
-#print background
-# This results in 7885.17828094 counts
+bckname = "Performance/CTA-Performance-North-" + options.hours + "h-Background.txt"
+areaname = "Performance/CTA-Performance-North-" + options.hours + "h-EffArea.txt"
+eblname = 'EBL_Gilmore.txt'
 
-background = 7885.17828094
-    
-# I asssume I know I have 80 energy bins
+ebl = EBL.EBL(eblname)
 
-filename = "CTA-Performance-North-5h-EffArea.txt"
-EffArea = np.loadtxt(filename)
+bckg = np.loadtxt(bckname)
+background = 0
+for i in range(0,len(bckg)):
+    background = background + bckg[i][2] * 3600 * float(options.hours)
+# ->>> This results in  7885.17828094 counts in the 5 hour search
+# ->>> This results in 44411.1408094 counts in the 50 hour search
 
-# Generate spectrum as a numpy array
-# Begin with 1e-12 (E/TEV)^(-2.1) 1/(TeV.cm^2.s)
-# This is a very bright spectrum approximately (but not exactly) that of a
-# single point source responsible for the IceCube diffuse flux
+EffArea = np.loadtxt(areaname)
 
 spectrum = np.empty([80,1])
 energy = np.empty([80,1])
 area = np.empty([80,1])
 delta = np.empty([80,1])
 
-redshift = 0.3
-
 signal = 0
+# HACK ALERT!!!!! I integrate only the first 77 entries, to ensure the energy is 100 or lower
+# The EBL attenuation is not available above 100 TeV
 for i in range(0,77):
-    spectrum[i] = 1e-12* math.pow(EffArea[i,0],-2.1) * math.exp(-ebl.TAU(EffArea[i,0],redshift))
+#   Generate spectrum as a numpy array
+#   Begin with 1e-12 (E/TEV)^(-2.1) 1/(TeV.cm^2.s)
+#   This is a very bright spectrum approximately (but not exactly) that of a
+#   single point source responsible for the IceCube diffuse flux
+    spectrum[i] = options.fluxnorm * math.pow(EffArea[i,0],options.index) * math.exp(-ebl.TAU(EffArea[i,0],options.redshift))
     energy[i] = EffArea[i][0]
     area[i] = EffArea[i][1]
     lowlog = math.log10(energy[i])-0.025
@@ -49,7 +57,7 @@ for i in range(0,77):
     # 10,000 to convert to cm^2
     # Delta is binwidth
     # 3600 * 5 seconds of signal
-    signal = signal + spectrum[i] * area[i] * 10000 * delta[i] * 3600 * 5
+    signal = signal + spectrum[i] * area[i] * 10000 * delta[i] * 3600 * float(options.hours)
 print "Signal", signal, " Background", background
 
 print "Significance", signal/math.sqrt(background)
