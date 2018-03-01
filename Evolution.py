5ddd6e5642852c6f2ae337a9838647cbc6e55301
René Reimann <renereimann@github.com>
2018-03-01 12:47:01 +0100

    changed evolution to class and sub-classes. Added class for source population, with all relevant member functions

--- Evolution.py
+++ Evolution.py
@@ -7,105 +7,153 @@ import scipy
 import cosmolopy
 cosmology = {'omega_M_0' : 0.308, 'omega_lambda_0' : 0.692, 'h' : 0.678}
 cosmology = cosmolopy.distance.set_omega_k_0(cosmology) #Flat universe
-
-# This is the redshift distribution for arbitrary evolution for either transient or steady sources 
-def RedshiftDistribution(z,options):
-    if options.Transient == False:
-        return 4*np.pi* Evolution(np.log10(1.+z),options.Evolution) * cosmolopy.distance.diff_comoving_volume(z,**cosmology)
-    else:
-        return 4*np.pi* Evolution(np.log10(1.+z),options.Evolution) * 1./(1.+z) * cosmolopy.distance.diff_comoving_volume(z,**cosmology)
       
-def Evolution(x,evol):
-    if (evol=="HB2006SFR"):
-        return HopkinsBeacom2006StarFormationRate(x)
-    elif (evol=="NoEvolution"):
-        return NoEvolution(x)
-    elif (evol=="CC2015SNR"):
-        return CandelsClash2015SNRate(x)
-    elif (evol=="YMKBH2008SFR"):
-        return YukselMatthewKistlerBeacomHopkins2008StarFormationRate(x)
-    else:
-        print "Source evolution " +  evol +  " not recognized"
-        quit()
+def get_evolution(evol):
+    evolutions = { "NoEvolution": Evolution_NoEvolution,
+                   "HB2006SFR": Evolution_HopkinsBeacom2006StarFormationRate,
+                   "YMKBH2008SFR": Evolution_YukselMatthewKistlerBeacomHopkins2008StarFormationRate,
+                   "CC2015SNR": Evolution_CandelsClash2015SNRate,
+                 }
+    if not evol in evolutions.keys():
+        raise NotImplementedError("Source evolution " + evol + " not implemented.")
+        
+    return evolutions[evol]()
 
-def YukselMatthewKistlerBeacomHopkins2008StarFormationRate(x):
+class Evolution():
+    def __init__():
+        pass
+        
+    def parametrization(self, x):
+        raise NotImplementedError("Abstract")
+
+    def __call__(self, x):
+        return self.parametrization(x)
+
+class Evolution_NoEvolution(Evolution):
+    def parametrization(self, x):
+        return 1.
+  
+class Evolution_HopkinsBeacom2006StarFormationRate(Evolution):
+    """ StarFormationHistory (SFR), from Hopkins and Beacom 2006, unit = M_sun/yr/Mpc^3 """
+     
+    def parametrization(self, x):
+        if x<0.30963:
+            return np.power(10,3.28*x-1.82)
+        if (x>=0.30963)and(x<0.73878):
+            return np.power(10,-0.26*x-0.724)
+        if x>=0.73878:
+            return np.power(10,-8.0*x+4.99)
+
+class Evolution_YukselMatthewKistlerBeacomHopkins2008StarFormationRate(Evolution):
     """ Star Formation Rate in units of M_sun/yr/Mpc^3
-	arXiv:0804.4008  Eq.5
+    arXiv:0804.4008  Eq.5
     """
-    z_plus_1 = 10**x
-    a = 3.4
-    b = -0.3
-    c = -3.5
-    # z1 = 1
-    # z2 =4 
-    B = 5160.63662037 # precomputed B = (1+z1)**(1-a/b)
-    C = 9.06337604231 # precomputed C = (1+z1)**((b-a)/c) * (1 + z2)**(1-b/c)
-    eta = -10
-    r0 = 0.02
-    return r0 * ( z_plus_1**(a*eta) + (z_plus_1/B)**(b*eta) + (z_plus_1/C)**(c*eta) )**(1./eta)
+        
+    def parametrization(self, x):
+        z_plus_1 = 10**x
+        a = 3.4
+        b = -0.3
+        c = -3.5
+        # z1 = 1
+        # z2 =4 
+        B = 5160.63662037 # precomputed B = (1+z1)**(1-a/b)
+        C = 9.06337604231 # precomputed C = (1+z1)**((b-a)/c) * (1 + z2)**(1-b/c)
+        eta = -10
+        r0 = 0.02
+        return r0 * ( z_plus_1**(a*eta) + (z_plus_1/B)**(b*eta) + (z_plus_1/C)**(c*eta) )**(1./eta)
+    
+class Evolution_CandelsClash2015SNRate(Evolution):
+    def parametrization(self, x):
+        a = 0.015
+        b = 1.5
+        c = 5.0
+        d = 6.1
+        density = a*(10.**x)**c / ((10.**x / b)**d+1.)
+        return density
 
-#StarFormationHistory (SFR), from Hopkins and Beacom 2006, unit = M_sun/yr/Mpc^3 
-def HopkinsBeacom2006StarFormationRate(x):
-    if x<0.30963:
-        return np.power(10,3.28*x-1.82)
-    if (x>=0.30963)and(x<0.73878):
-        return np.power(10,-0.26*x-0.724)
-    if x>=0.73878:
-        return np.power(10,-8.0*x+4.99)
+class SourcePopulation():
+    def __init__(self, cosmology, evolution):
+        self.evolution = evolution
+        self.cosmology = cosmolopy.distance.set_omega_k_0(cosmology)
+        self._zlocal = 0.01
+        self.Mpc2cm = 3.086e24                    # Mpc / cm
+        self.GeV_per_sec_2_ergs_per_year = 50526  # (GeV/sec) / (ergs/yr)
+        self.dL1 = self.LuminosityDistance(1.)
+        
+    def RedshiftDistribution(self, z):
+        """ can remove 4*pi becaue we just use this in a normalized way """
+        return 4*np.pi* self.evolution(np.log10(1.+z)) * cosmolopy.distance.diff_comoving_volume(z, **self.cosmology)
+        
+    def RedshiftIntegral(self, zmax):
+        """ $$ \int_0^{z_\mathrm{max}} \frac{\mathrm{d}N}{\mathrm{d}z} \,\mathrm{d}V_c(z) \,\mathrm{d}z $$ """
+        
+        return scipy.integrate.quad(lambda z: self.RedshiftDistribution(z), 0, zmax)[0]
+        
+    def LuminosityDistance(self, z):
+        # Wrapper function - so that cosmolopy is only imported here.
+        return cosmolopy.distance.luminosity_distance(z, **self.cosmology)
+        
+    def Nsources(density, zmax):
+        """ Total number of sources within $z_\mathrm{max}$:
 
-def NoEvolution(x):
-    return 1.
+        $$ N_\mathrm{tot} = \rho\cdot V_c(z=0.01) \frac{\int_0^{z_\mathrm{max}} \frac{\mathrm{d}N}{\mathrm{d}z} V_c(z) \,\mathrm{d}z}{\int_0^{0.01} \frac{\mathrm{d}N}{\mathrm{d}z} V_c(z) \,\mathrm{d}z} $$
+        """
+        vlocal = cosmolopy.distance.comoving_volume(self._zlocal, **self.cosmology)
+        Ntotal = density * vlocal / (self.RedshiftIntegral(zlocal) / 
+                                     self.RedshiftIntegral(zmax) )
+        return Ntotal
+        
+    def Flux2Lumi(fluxnorm, index, emin, emax, E0=1e5):
+        """
+        $$ L_\nu = \frac{ \Phi_{z=1}^{PS} }{E_0^2} \int_{E_\mathrm{min}}^{E_\mathrm{max}} E \left(\frac{E}{E_0}\right)^{-\gamma}\,\mathrm{d}E\,4\pi d_L^2(z=1) $$
 
-def CandelsClash2015SNRate(x):
-    a = 0.015
-    b = 1.5
-    c = 5.0
-    d = 6.1
-    density = a*(10.**x)**c / ((10.**x / b)**d+1.)
-    return density
+        Note fluxnorm is E0^2*fluxnorm 
+        fluxnorm units are []
+        """
+        flux_integral = scipy.integrate.quad(lambda E: E*(E/E0)**(-abs(index)), emin, emax)[0]
+        luminosity = fluxnorm / E0**2. * flux_integral * self.GeV_per_sec_2_ergs_per_year * 4. * np.pi * (self.dL1*self.Mpc2cm)**2. 
 
-#class Evolution():
-#    def __init__():
-#        pass
-#        
-#    def Evolution
-#    
-#    def RedshiftDistribution(z):
-#        pass
+        return luminosity    
+        
+    def Lumi2Flux(luminosity, index, emin, emax, E0=1.e5):
+        """  
+        $$ L_\nu = \frac{ \Phi_{z=1}^{PS} }{E_0^2} \int_{E_\mathrm{min}}^{E_\mathrm{max}} E \left(\frac{E}{E_0}\right)^{-\gamma}\,\mathrm{d}E\,4\pi d_L^2(z=1) $$
 
-def StandardCandleSources(options):
-  norm = scipy.integrate.quad(lambda z: RedshiftDistribution(z, options), 0, options.zmax)[0]
-  area = scipy.integrate.quad(lambda z: RedshiftDistribution(z, options), 0, 0.01)[0]
+        Lumi given in ergs/yr
+        Note fluxnorm is E0^2*fluxnorm 
+        fluxnorm units are []
+        """
+        flux_integral = scipy.integrate.quad(lambda E: E*(E/E0)**(-abs(index)), emin, emax)[0]
+        fluxnorm = luminosity / 4. / np.pi / (self.dL1*self.Mpc2cm)**2. / self.GeV_per_sec_2_ergs_per_year / flux_integral * E0**2. 
+        return fluxnorm
+ 
+    def StandardCandleSources(fluxnorm, density, zmax, index):
+        """ $$ \Phi_{z=1}^{PS} = \frac{4 \pi \Phi_\mathrm{diffuse}}{N_\mathrm{tot}\,d_L^2(z=1)\, \int_0^{10} \frac{ (1+z)^{-\gamma+2} }{d_L(z)^2}  \frac{\frac{\mathrm{d}N}{\mathrm{d}z}    V_c(z)}{ \int_0^{z_\mathrm{max}} \frac{\mathrm{d}N}{\mathrm{d}z'} V_c(z') \,\mathrm{d}z'} \,\mathrm{d}z} $$                                         
+        """
+        norm = self.RedshiftIntegral(zmax)
+        Ntotal = self.Nsources(density, zmax)
+        all_sky_flux = 4 * np.pi * fluxnorm
+      
+        # Here the integral on redshift is done from 0 to 10.
+        # This insures proper normalization even if options.zmax is not 10.
+        Fluxnorm = all_sky_flux / Ntotal / self.dL1**2. / scipy.integrate.quad(lambda z: (1.+z)**(-abs(index)+2) / (self.LuminosityDistance(z)**2.) * self.RedshiftDistribution(z) / norm, 0,10.)[0]
 
-  vlocal = cosmolopy.distance.comoving_volume(0.01, **cosmology)
-  Ntotal = options.density * vlocal / (area/norm)
-  dL1 = cosmolopy.distance.luminosity_distance(1.0, **cosmology)
-  if options.Transient != True:
-    # Here the integral on redshift is done from 0 to 10.
-    # This insures proper normalization even if options.zmax is not 10.
-    Fluxnorm = 4*np.pi*options.fluxnorm / scipy.integrate.quad(lambda z: Ntotal*dL1**2./(LuminosityDistance(z)**2.)*RedshiftDistribution(z, options)/norm*((1.+z)/2.)**(-options.index+2), 0,10.)[0]
-  else:
-    #For transient source, Fluxnorm will be the fluence of a standard candle at z=1, with unit GeV/cm^2 given that the burst rate density is measured in per year.
-    # As above, the integral is done from redshift 0 to 10.
-    Fluxnorm = 4*np.pi*options.fluxnorm*86400*365 / scipy.integrate.quad(lambda z: Ntotal*dL1**2./(LuminosityDistance(z)**2.)*RedshiftDistribution(z, options)/norm*((1.+z)/2.)**(-options.index+3), 0,10.)[0]
-  return [int(Ntotal), Fluxnorm] 
+        return Fluxnorm
+    
+class TransientSourcePopulation(SourcePopulation):
+        
+    def RedshiftDistribution(z):
+        return super(TransientSourcePopulation, self).RedshiftDistribution(z) * 1./(1.+z) 
 
-# Wrapper fucntion - so that cosmolopy is only imported here.
-def LuminosityDistance(z):
-    return cosmolopy.distance.luminosity_distance(z, **cosmology)
-
-# Convert luminosity of standard candle to diffuse neutrino flux and standard candle flux
-def LtoFlux(options):
-  #change unit of luminosity from erg/yr to GeV/s
-  l = 1.97917e-5*options.luminosity
-  #change energy luminosity to particle luminosity, integrating the particle luminosity from 10TeV to 10 PeV
-  m0 = l / scipy.integrate.quad(lambda E: E*(E/1.e5)**(-options.index), 1e4, 1e7)[0]
-  #calculate the normalization for particle spectrum of source at z = 1 
-  candleflux = 2.**(2.-options.index)*m0/4./np.pi/(LuminosityDistance(1)*3.086e24)**2. * (1.e5)**2
-  return candleflux
-
-
-
-
-
-
+    def StandardCandleSources(fluxnorm, density, zmax, index):
+        # For transient source, Fluxnorm will be the fluence of a standard candle at z=1, with unit GeV/cm^2 given that the burst rate density is measured in per year.
+      
+        norm = self.RedshiftIntegral(zmax)
+        Ntotal = self.Nsources(density, zmax)
+        yr2sec = 86400*365
+        all_sky_flux = 4 * np.pi * fluxnorm * yr2sec
+      
+        # As above, the integral is done from redshift 0 to 10.
+        fluence = all_sky_flux / Ntotal / self.dL1**2. / scipy.integrate.quad(lambda z: (1.+z)**(-abs(index)+3) / (self.LuminosityDistance(z)**2.) * self.RedshiftDistribution(z) / norm, 0,10.)[0]
+      
+        return fluence
