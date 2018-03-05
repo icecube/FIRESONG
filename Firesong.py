1042f6485ca837ce049321a59b17c7e027af3788
René Reimann <renereimann@github.com>
2018-03-05 23:30:27 +0100

    clean up and argument parsing

--- Firesong.py
+++ Firesong.py
@@ -6,8 +6,6 @@
 # General imports
 from __future__ import division
 import os
-import gzip
-import re
 import argparse
 # Numpy / Scipy
 import numpy as np
@@ -17,51 +15,61 @@ from Evolution import TransientSourcePopulation, cosmology, Simulation
 from Luminosity import get_LuminosityFunction
 from input_output import output_writer, print_str
 
-def firesong_simulation(options, outputdir):
-    emin = 1e4
-    emax = 1e7
 
-    if options.Transient is True:
+def firesong_simulation(outputdir,
+                        filename='Firesong.out',
+                        density=1e-9,
+                        Evolution="HB2006SFR",
+                        Transient=False,
+                        timescale=1000.,
+                        zmax=10.,
+                        fluxnorm=0.9e-8,
+                        index=2.13,
+                        LF="SC",
+                        sigma=1.0,
+                        zNEAR=-1,
+                        luminosity=0.0,
+                        emin=1e4,
+                        emax=1e7):
+
+    if Transient:
         population = TransientSourcePopulation(cosmology,
-                                               get_evolution(options.Evolution),
-                                               timescale=options.timescale)
+                                               get_evolution(Evolution),
+                                               timescale=timescale)
     else:
-        population = SourcePopulation(cosmology,
-                                      get_evolution(options.Evolution))
+        population = SourcePopulation(cosmology, get_evolution(Evolution))
 
-    N_sample = int(population.Nsources(options.density, options.zmax))
+    N_sample = int(population.Nsources(density, zmax))
 
-    if options.luminosity == 0.0:
+    if luminosity == 0.0:
         ## If luminosity not specified calculate luminosity from diffuse flux
-        luminosity = population.StandardCandleLuminosity(options.fluxnorm,
-                                                         options.density,
-                                                         options.zmax,
-                                                         options.index,
+        luminosity = population.StandardCandleLuminosity(fluxnorm,
+                                                         density,
+                                                         zmax,
+                                                         index,
                                                          emin=emin,
                                                          emax=emax)
-    else:
-        luminosity = options.luminosity
 
-    delta_gamma = 2-options.index
-    print_str(options.LF, options.Transient, options.timescale,
-              options.Evolution, options.density, N_sample,
-              options.luminosity, options.fluxnorm, delta_gamma,
-              options.zmax, luminosity)
+    delta_gamma = 2-index
+    print_str(LF, Transient, timescale, Evolution, density, N_sample,
+              luminosity, fluxnorm, delta_gamma, zmax, luminosity)
 
     ##################################################
     #        Simulation starts here
     ##################################################
 
     simulation = Simulation(population,
-                            get_LuminosityFunction(luminosity, LF=options.LF, sigma=options.sigma),
-                            index=options.index,
-                            zmax=options.zmax,
+                            get_LuminosityFunction(luminosity,
+                                                   LF=LF,
+                                                   sigma=sigma),
+                            index=index,
+                            zmax=zmax,
                             emin=emin,
                             emax=emax)
 
-    out = output_writer(outputdir, options.filename)
-    out.write_header(options.LF, options.Transient, options.timescale,
-                     options.fluxnorm, delta_gamma, options.luminosity)
+    out = output_writer(outputdir, filename)
+    out.write_header(LF, Transient, timescale, fluxnorm,
+                     delta_gamma, luminosity)
 
     # CHECK loop can be more efficient
     TotalFlux = 0
@@ -74,7 +82,7 @@ def firesong_simulation(options, outputdir):
 
         # For transient sources, the flux measured on Earth will be
         # red-shifted-fluence/{(1+z)*burst duration}
-        if options.Transient:
+        if Transient:
             flux = population.fluence2flux(flux, z)
 
         out.write(declin, z, flux)
@@ -83,45 +91,17 @@ def firesong_simulation(options, outputdir):
 
     # For transient source, we calculate the total fluence from all sources,
     # then obtain the diffuse flux by doing a time average over a year
-    if options.Transient:
+    if Transient:
         TotalFlux = TotalFlux / population.yr2sec
     TotalFlux /= 4*np.pi  # give in per sr
 
     out.finish(TotalFlux)
-    print "Actual diffuse flux simulated :  E^2 dNdE = {TotalFlux} (E/100 TeV)^({delta_gamma}) [GeV/cm^2.s.sr]".format(**locals())
-
-
-def firesong_simulation_by_hand(outputdir,
-                                filename='Firesong.out',
-                                density=1e-9,
-                                Evolution="HB2006SFR",
-                                Transient=False,
-                                timescale=1000.,
-                                zmax=10.,
-                                fluxnorm=0.9e-8,
-                                index=2.13,
-                                LF="SC",
-                                sigma=1.0,
-                                zNEAR=-1,
-                                luminosity=0.0):
-
-    options = argparse.Namespace(filename=filename,
-                                 density=density,
-                                 Evolution=Evolution,
-                                 Transient=Transient,
-                                 timescale=timescale,
-                                 zmax=zmax,
-                                 fluxnorm=fluxnorm,
-                                 index=index,
-                                 LF=LF,
-                                 sigma=sigma,
-                                 zNEAR=zNEAR,
-                                 luminosity=luminosity)
-    firesong_simulation(options, outputdir)
+    print "Actual diffuse flux simulated :"
+    log = "E^2 dNdE = {TotalFlux} (E/100 TeV)^({delta_gamma}) [GeV/cm^2.s.sr]"
+    print log.format(**locals())
 
 if __name__ == "__main__":
 
-    #
     # Check that the Firesong environmental variable is set
     # This is needed for output and to read exposures, effective areas, etc
     try:
@@ -131,9 +111,7 @@ if __name__ == "__main__":
         quit()
     outputdir = firesongdir + "/Results/"
 
-    #
     # Process command line options
-    #
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', action='store',
                         dest='filename',
@@ -144,7 +122,7 @@ if __name__ == "__main__":
                         help='Local neutrino source density [1/Mpc^3]')
     parser.add_argument("--evolution", action="store",
                         dest="Evolution", default='HB2006SFR',
-                        help="Source evolution options:  HB2006SFR (default),  NoEvolution")
+                        help="Source evolution options:  HB2006SFR (default), NoEvolution")
     parser.add_argument("--transient", action='store_true',
                         dest='Transient', default=False,
                         help='Simulate transient sources, NOT TESTED YET!')
@@ -171,7 +149,18 @@ if __name__ == "__main__":
     parser.add_argument("--L", action="store",
                         dest="luminosity", type=float, default=0.0,
                         help="Set luminosity for each source, will reset fluxnorm option, unit erg/yr")
-
     options = parser.parse_args()
 
-    firesong_simulation(options, outputdir)
+    firesong_simulation(outputdir,
+                        filename=options.filename,
+                        density=options.density,
+                        Evolution=options.Evolution,
+                        Transient=options.Transient,
+                        timescale=options.timescale,
+                        zmax=options.zmax,
+                        fluxnorm=options.fluxnorm,
+                        index=options.index,
+                        LF=options.LF,
+                        sigma=options.sigma,
+                        zNEAR=options.zNEAR,
+                        luminosity=options.luminosity)
