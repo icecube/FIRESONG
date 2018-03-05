5eaae6a364903024e696bb7007671f75f0279f18
René Reimann <renereimann@github.com>
2018-03-05 16:29:39 +0100

    documented Luminosity.py and give meaning full names and doc strings

--- Luminosity.py
+++ Luminosity.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
-#
 
+# imports
 import numpy as np
 from scipy.stats import lognorm
 
 
 class LuminosityFunction(object):
-    def __init__(self, candleflux):
-        self.meanflux = candleflux              # mean flux
+    """Abstract Luminosity Function class.
+    Defines the minimal requirements for a Luminosity Function class"""
+
+    def __init__(self, mean_luminosity):
+        """Luminosity function.
+
+        Parameters:
+            - mean luminosity (Not not median and not log(mean))
+        """
+        self.mean = mean_luminosity
 
     def sample_distribution(self, nsources, rng=None):
         raise NotImplementedError("Abstract Class")
@@ -18,35 +26,78 @@ class LuminosityFunction(object):
     def cdf(self):
         raise NotImplementedError("Abstract Class")
 
-    def cdf_eval(self):
-        raise NotImplementedError("Abstract Class")
-
 
 class SC_LuminosityFunction(LuminosityFunction):
+    """ Standard Candle Luminosity functions.
+    All sources have same luminosity.
+    """
 
     def sample_distribution(self, nsources=None, rng=None):
-        return self.meanflux
+        """ Samples from the Luminosity Function nsource times
+
+        Parameters:
+            number of sources
+        """
+        return self.mean
 
     def pdf(self, lumi):
-        raise NotImplementedError("""This is a delta function.
-            PDF not implemented.""")
+        """ Gives the value of the PDF at lumi.
+
+        Parameters:
+            lumi: float or array-like, point where PDF is evaluated.
+
+        Notes:
+            PDF for SC not defined. This just works if an array is given.
+            PDF is zero every where except where bin edges include the SC flux.
+        """
+        lumi = np.atleast_1d(lumi)
+        if len(lumi) < 2:
+            raise NotImplementedError("""This is a delta function.
+                PDF not defined.
+                We only can return something meaning full for a nice array.
+                """)
+        pdf = np.zeros_like(lumi)
+        pdf[:-1][np.logical_and(self.mean > lumi[:-1],
+                 self.mean < lumi[1:])] = 1
+        return pdf
 
     def cdf(self, lumi):
-        raise NotImplementedError("""This is a delta function.
-            CDF not implemented.""")
+        """ Gives the value of the CDF at lumi.
 
-    def cdf_eval(self):
-        return 1
+        Parameters:
+            lumi: float or array-like, point where CDF is evaluated.
+
+        Notes:
+            0    lumi < mean
+            0.5  lumi = mean
+            1    lumi > mean
+        """
+        cdf = np.zeros_like(lumi)
+        cdf[lumi == self.mean] = .5
+        cdf[lumi > self.mean] = 1.
+        return cdf
 
 
 class LG_LuminosityFunction(LuminosityFunction):
 
-    def __init__(self, candleflux, width):
-        self.meanflux = candleflux
+    def __init__(self, mean_luminosity, width):
+        """ Log Normal Luminosity function.
+
+        Parameters:
+            - mean luminosity (Not not median and not log(mean))
+            - width in dex (width in log10)
+
+        Note:
+            Relation between mean and median luminosity:
+            log(median) = log(mean) - sigma^2/2
+            Relation between width and simga:
+            sigma = log(10^width)
+        """
+        self.mean = mean_luminosity
         self.width = width                     # width is given in log10
-        self.logmean = np.log(self.meanflux)   # log mean flux
+        self.logmean = np.log(self.mean)       # log mean luminosity
         self.sigma = np.log(10**self.width)    # sigma is given in ln
-        self.mu = self.logmean-self.sigma**2./2.  # log median flux
+        self.mu = self.logmean-self.sigma**2./2.  # log median luminosity
 
     def sample_distribution(self, nsource=None, rng=None):
         """ Samples from the Luminosity Function nsource times
@@ -87,19 +138,10 @@ class LG_LuminosityFunction(LuminosityFunction):
         """
         return lognorm.cdf(lumi, s=self.sigma, scale=np.exp(self.mu))
 
-    def cdf_eval(self):
-        """ Evaluates CDF at within a `central` range.
-        Returns luminosity list and CDF values
-        corresponding to these luminosity values.
-        """
-
-        f1_list = np.exp(np.linspace(self.mu-3*self.sigma,
-                                     self.mu+3*self.sigma, 10000))
-        return f1_list, self.cdf(f1_list)
-
 
 class PL_LuminosityFunction(LuminosityFunction):
-    """
+    """ Power-law distribution defined between x_min and x_max.
+
         PDF:
                        (1-alpha)
             ------------------------------------ * x^(-alpha)
@@ -122,27 +164,30 @@ class PL_LuminosityFunction(LuminosityFunction):
         SFI Working Paper: 2007-12-049 (2007)  arxiv:0706.1062
     """
 
-    def __init__(self, candleflux, index, width):
+    def __init__(self, mean_luminosity, index, width):
+        """ Power-law distribution defined between x_min and x_max.
+        x_min and x_max are calculated from mean and width of distribution.
+
+        Note:
+            f_mean ~= width*ln(10)*F_min,          index = -2
+            f_mean ~= (index+1)/(index+2)*F_min    index < -2
         """
-            index = -2, f_mean ~= width*ln(10)*F_min
-            index < -2, f_mean ~= (index+1)/(index+2)*F_min
-        """
-        self.meanflux = candleflux
+        self.mean = mean_luminosity
         self.index = index
         self.width = width
 
         if self.index == -2:
-            self.Fmin = self.meanflux / (self.width*np.log(10))
+            self.Fmin = self.mean / (self.width*np.log(10))
         else:
-            self.Fmin = (self.index+2.)/(self.index+1.)*self.meanflux
+            self.Fmin = (self.index+2.)/(self.index+1.)*self.mean
 
         self.Fmax = self.Fmin*10**self.width
 
     def sample_distribution(self, nsource=None, rng=None):
-        """
-        inv.CDF:
-        a = 1-index
-        P^{-1}(x)=(x_min^a+(x_max^a-x_min^a)*x)^(1/a)
+        """Samples from the Luminosity Function nsource times
+
+        Parameters:
+            number of sources
         """
         if rng is None:
             rng = np.random.RandomState()
@@ -152,8 +197,13 @@ class PL_LuminosityFunction(LuminosityFunction):
                                    self.Fmin**beta)*x)**(1./beta)
 
     def pdf(self, lumi):
-        """
-        PDF:
+        """Gives the value of the PDF at lumi.
+
+        Parameters:
+            lumi: float or array-like, point where PDF is evaluated.
+
+        Notes:
+            PDF given by:
                        (1-alpha)
             ------------------------------------ * x^(-alpha)
             (x_max^(1-alpha) - x_min^(1-alpha))
@@ -166,8 +216,13 @@ class PL_LuminosityFunction(LuminosityFunction):
         return pdf
 
     def cdf(self, lumi):
-        """
-        CDF:
+        """Gives the value of the CDF at lumi.
+
+        Parameters:
+            lumi: float or array-like, point where CDF is evaluated.
+
+        Note:
+            CDF given by:
              x^(1-alpha) - x_min^(1-alpha)
             ---------------------------------
             x_max^(1-alpha) - x_min^(1-alpha)
@@ -175,19 +230,29 @@ class PL_LuminosityFunction(LuminosityFunction):
         return (lumi**(1+self.index) - self.Fmin**(1+self.index)) / \
                ((self.Fmax**(1+self.index) - self.Fmin**(1+self.index)))
 
-    def cdf_eval(self):
-        f1_list = np.logspace(np.log10(self.Fmin), np.log10(self.Fmax), 10000)
-        return f1_list, self.cdf(f1_list)
 
+def get_LuminosityFunction(options, mean_luminosity):
+    """Returns a Luminosity Function based on an abbreviation.
+    Known abbreviations are:
+        - SC - Standard Candle
+        - LG - Log-Normal
+        - PL - Power-Law
 
-def get_LuminosityFunction(options, candleflux):
+    Parameters:
+        - options: Namespace with LF and needed parameters to
+                   construct luminosity function.
+        - mean_luminosity: mean luminosity.
+
+    Raises 'NotImplementedError' for unknown abbreviation.
+    """
+
     if options.LF == "SC":
-        return SC_LuminosityFunction(candleflux)
+        return SC_LuminosityFunction(mean_luminosity)
     if options.LF == "LG":
-        return LG_LuminosityFunction(candleflux,
+        return LG_LuminosityFunction(mean_luminosity,
                                      options.sigma)
     if options.LF == "PL":
-        return PL_LuminosityFunction(candleflux,
+        return PL_LuminosityFunction(mean_luminosity,
                                      options.index,
                                      options.sigma)
     raise NotImplementedError("The luminosity function " +
