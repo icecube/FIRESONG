16ce1b9a1a064ae79c32a7e92c45a1abc4987971
Konstancja Satalecka <konstancja.satalecka@desy.de>
2018-03-15 13:39:38 +0100

    Add files via upload
    Adapted to new CTA IRFs from Prod3b. Now uses the CTAsensitivity_LiMa.py to calculate significance.

--- CTA/CTA-Firesong.py
+++ CTA/CTA-Firesong.py
@@ -7,7 +7,7 @@ import glob
 import subprocess
 import re
 import argparse
-import CTAsensitivity
+import CTAsensitivity_LiMa
 
 #### HARDCODE ALERT
 parser = argparse.ArgumentParser()
@@ -22,23 +22,36 @@ parser.add_argument("--transient", action='store_true',
                     help='Simulate transient sources, NOT TESTED YET!')
 parser.add_argument("--timescale", action='store', dest='timescale', type=float,
                     default=1000., help='time scale of transient sources, default is 1000sec.')
+parser.add_argument("--array", action='store', dest='array', type=str,
+                    default='North', help='Choose between the CTA North or South, and between the BL and TS arrays: North, North-TS, South, South-TS')
+parser.add_argument("--zenith", action='store', dest='zenith', type=str,
+                    default='20deg', help='Choose between low zenith 20deg and high zenith 40deg observations with CTA')
 options = parser.parse_args()
 
 try:
     alerts = open(options.input)
 except:
-    print "Couldn't open " + options.input
+    print("Couldn't open " + options.input)
     quit()
 try:
     output = open(options.output,"w")
 except:
-    print "Couldn't open" + options.output
+    print("Couldn't open" + options.output)
     quit()
         
 Observed05 = 0
 Observed5 = 0
 Observed50 = 0
 Neutrinos = 0
+# CTA prod3b IRFs for 30 minutes observation
+bckname_30m = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-30m_20170627_Background.txt"
+areaname_30m = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-30m_20170627_EffArea.txt"
+# CTA prod3b IRFs for 5 hours observation
+bckname_5h = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-05h_20170627_Background.txt"
+areaname_5h = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-05h_20170627_EffArea.txt"
+# CTA prod3b IRFs for 50 hours observation
+bckname_50h = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-50h_20170627_Background.txt"
+areaname_50h = "Performance/Prod3b/CTA-Performance-" +options.array+ "-" + options.zenith + "-average-50h_20170627_EffArea.txt"
 
 for neutrino in alerts:
     if re.search("#",neutrino):
@@ -51,25 +64,25 @@ for neutrino in alerts:
 # These should be undetectable anyway
 # It'd be better to make CTA-Sensisitivity in to a library
     if (z<9): 
-        significance = CTAsensitivity.Significance(f, z, "0.5", options)
+        significance = CTAsensitivity_LiMa.Significance(f, z, 0.5, bckname_30m, areaname_30m, options)
         if float(significance)>5:
             Observed05 = Observed05+1
-            output.write('{:.3f} {:.4f} {:.6e}\n'.format(dec, z, f))
-        significance = CTAsensitivity.Significance(f, z, "5", options)    
+            #output.write('{0:.3f} {1:.4f} {2:.6e} {3:.2f}\n'.format(dec, z, f, significance))
+        significance = CTAsensitivity_LiMa.Significance(f, z, 5., bckname_5h, areaname_5h, options)    
         if significance>5:
             Observed5 = Observed5+1
-            output.write('{:.3f} {:.4f} {:.6e}\n'.format(dec, z, f))
-        significance = CTAsensitivity.Significance(f, z, "50", options)    
+            #output.write('{0:.3f} {1:.4f} {2:.6e} {3:.2f}\n'.format(dec, z, f, significance))
+        significance = CTAsensitivity_LiMa.Significance(f, z, 50., bckname_50h, areaname_50h, options)    
         if significance>5:
             Observed50 = Observed50+1
-            output.write('{:.3f} {:.4f} {:.6e}\n'.format(dec, z, f))
+            #output.write('{0:.3f} {1:.4f} {2:.6e} {3:.2f}\n'.format(dec, z, f, significance))
     Neutrinos = Neutrinos +1
     if Neutrinos%50==0:
-        print "Processed Neutrinos:", Neutrinos
+        print("Processed Neutrinos:", Neutrinos)
 Prob05 = Observed05/Neutrinos
 Prob5 = Observed5/Neutrinos
 Prob50 = Observed50/Neutrinos
-output.write('{:4f} {:4f} {:4f} \n'.format(Prob05,Prob5,Prob50))
+output.write('{0:4f} {1:4f} {2:4f} \n'.format(Prob05,Prob5,Prob50))
 
 alerts.close()
 output.close()
