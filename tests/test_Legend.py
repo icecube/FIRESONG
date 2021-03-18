81b25d43565bfc55d6060c188db0b67245a16c69
Alex Pizzuto <pizzuto@wisc.edu>
2021-03-18 10:43:51 -0500

    Add setup and restructure dirs

--- tests/test_Legend.py
+++ tests/test_Legend.py
@@ -56,8 +56,8 @@ class TestLegendSimulation(unittest.TestCase):
             places=1)
 
     def test_total_flux(self):
-        self.assertAlmostEqual(np.log10(np.sum(self.legend_uni['flux'])/7.117604312572643e-06),
-            0.0,
+        self.assertAlmostEqual(np.log10(np.sum(self.legend_uni['flux'])/7.117604312572643e-06) + 1.,
+            1.0,
             places=1)
 
 if __name__ == "__main__":
