c6ca5d4977278f8d78c72b3eba33728d59b0a2a7
Alex Pizzuto <pizzuto@wisc.edu>
2021-04-22 11:26:21 -0500

    consistent version numbers in setup, include python version in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 long_message = 'FIRESONG: the FIRst Extragalactic Simulation Of Neutrinos and Gamma-rays'
-version = "0.0.1"
+version = "1.6"
 
 setuptools.setup(
     name="firesong", 
@@ -17,5 +17,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires='>=3.1',
+    python_requires='>=3.7',
 )
