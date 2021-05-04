7129bd53cb44e93b8ee9aea17cb008f23d6f358b
ChrisCFTung <christung616@gmail.com>
2021-05-03 21:05:00 -0400

    Added workflow to publish on pypi

--- firesong/__init__.py
+++ firesong/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'C.F. Tung, T. Glauch, M. Larson, A. Pizzuto, R. Reimann, I. Taboada'
 __email__ = ''
-__version__ = '1.6.1'
+__version__ = '1.7'
 __all__ = ['Firesong', 'Evolution', 'distance', 'FluxPDF', 
            'input_out', 'Luminosity', 'sampling', 'Legend']
