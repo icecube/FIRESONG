6f976c4c0754b737c055e2f9a31cdcd6637f5042
ChrisCFTung <christung616@gmail.com>
2021-05-09 23:07:32 -0400

    version for JOSS pre-publish

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 long_message = 'FIRESONG: the FIRst Extragalactic Simulation Of Neutrinos and Gamma-rays'
-version = "1.7"
+version = "1.8"
 
 setuptools.setup(
     name="firesong", 
