a9971acbc29b7da466df769d38e9a999cf43c4a8
Alison Peisker <peiskera@msu.edu>
2021-06-07 11:11:36 -0400

    added documentation for conversion

--- firesong/ConvertFlux.py
+++ firesong/ConvertFlux.py
@@ -4,6 +4,13 @@ def nu_to_gamma(nuFlux, index, interaction):
 
     """
     Convert the neutrino flux to gamma-ray flux
+    The neutrino flux in GeV cm^-2 s^-1 at a pivot energy of 100 TeV
+    is converted to a gamma-ray flux in TeV^-1 cm^-2 s^-1 at a pivot
+    energy of 1 TeV.
+    There is a factor of 2 difference between the pp and pgamma cases
+    based on the ratio of charged to neutral pions.
+    The conversion equation and details can be found in the
+    reference: arXiv:1309.4077
 
     Args:
         nuFlux (float or array): neutrino flux in GeV cm^-2 s^-1
@@ -27,6 +34,13 @@ def gamma_to_nu(gammaFlux, index, interaction):
 
     """
     Convert gamma-ray flux to neutrino flux
+    The gamma-ray flux in TeV^-1 cm^-2 s^-1 at a pivot energy of 1 TeV
+    is converted to a neutrino flux in GeV cm^-2 s^-1 at a pivot
+    energy of 100 TeV.
+    There is a factor of 2 difference between the pp and pgamma cases
+    based on the ratio of charged to neutral pions.
+    The conversion equation and details can be found in the
+    reference: arXiv:1309.4077
 
     Args:
         gammaFlux (float or array): gamma-ray flux in TeV^-1 cm^-2 s^-1
