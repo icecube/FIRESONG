9f2dbcd9fe6c720f388016633702c72cbde2c41a
Chris Tung <christung616@gmail.com>
2022-07-07 21:15:12 -0400

    Update test_Luminosity.py
    match the number of digit return on github action

--- tests/test_Luminosity.py
+++ tests/test_Luminosity.py
@@ -84,7 +84,7 @@ class TestLuminosity(unittest.TestCase):
 
     def test_LG_LuminosityFunction_PDF(self):
         lf = Luminosity.LG_LuminosityFunction(1e50, 1.)
-        self.assertEqual(lf.pdf(1e49), 1.7128684087744686e-50)
+        self.assertEqual(lf.pdf(1e49), 1.712868408774468e-50)
 
     def test_LG_LuminosityFunction_CDF(self):
         lf = Luminosity.LG_LuminosityFunction(1e50, 1.)
